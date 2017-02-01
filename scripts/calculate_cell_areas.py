3570c264c6bd4c1370e81ab978d1706a294748b1
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2017-02-01 21:59:48 +0000

    Add naive implementation for producing heatmaps and output csv file

--- scripts/calculate_cell_areas.py
+++ scripts/calculate_cell_areas.py
@@ -11,6 +11,8 @@ import argparse
 from jicbioimage.core.image import Image
 from jicbioimage.core.io import AutoName, AutoWrite
 
+from jicbioimage.illustrate import Canvas
+
 from utils import (
     identity,
     invert,
@@ -25,13 +27,66 @@ __version__ = "0.3.0"
 AutoName.prefix_format = "{:03d}_"
 
 
+class LeafHeatmap(object):
+    """Class for working with cell areas."""
+
+    def __init__(self, segmentation):
+        self.segmentation = segmentation
+        self.identifiers = []
+        self.areas = []
+
+        for i in segmentation.identifiers:
+            self.identifiers.append(i)
+            region = segmentation.region_by_identifier(i)
+            self.areas.append(region.area)
+
+        self.min_area = min(self.areas)
+        self.max_area = max(self.areas)
+        self.range_area = self.max_area - self.min_area
+
+    def heatmap_rgb_from_area(self, area):
+        """Return heatmap colour."""
+        normalised_intensity = (area - self.min_area) / self.range_area
+        intensity = normalised_intensity * 255
+        intensity = int(round(intensity))
+        return (intensity, intensity, 255 - intensity)
+
+    def write_png(self, fpath):
+        height, width = self.segmentation.shape
+        canvas = Canvas.blank_canvas(width=width, height=height)
+        for i, area in zip(self.identifiers, self.areas):
+            color = self.heatmap_rgb_from_area(area)
+            region = self.segmentation.region_by_identifier(i)
+            canvas[region] = color
+        with open(fpath, "wb") as fh:
+            fh.write(canvas.png())
+
+
+def draw_heatmap_leaf(segmentation, output_directory):
+    png_fpath = os.path.join(output_directory, "heatmap_laef.png")
+    leaf_heatmap = LeafHeatmap(segmentation)
+    leaf_heatmap.write_png(png_fpath)
+
+
+def write_csv(segmentation, output_directory):
+    """Write out the cell areas to a csv file."""
+    csv_fpath = os.path.join(output_directory, "areas.csv")
+    with open(csv_fpath, "w") as fh:
+        fh.write("cell_id,area\n")
+        for i in segmentation.identifiers:
+            region = segmentation.region_by_identifier(i)
+            fh.write("{},{}\n".format(i, region.area))
+
+
 def analyse_file(image_fpath, mask_fpath, output_directory):
     """Analyse a single file."""
     logging.info("Analysing file: {}".format(image_fpath))
     image = Image.from_file(image_fpath)
 
     logging.info("Mask file: {}".format(mask_fpath))
-    mask = Image.from_file(mask_fpath)[:, :, 0]
+    mask = Image.from_file(mask_fpath)
+    if len(mask.shape) == 3:
+        mask = mask[:, :, 0]
     mask = identity(mask)
 
     seeds = invert(image)
@@ -39,6 +94,9 @@ def analyse_file(image_fpath, mask_fpath, output_directory):
     seeds = logical_and(seeds, mask)
     seeds = connected_components(seeds, connectivity=1)
     segmentation = watershed_with_seeds(-image, seeds, mask)
+
+    write_csv(segmentation, output_directory)
+    draw_heatmap_leaf(segmentation, output_directory)
 
 
 def main():
